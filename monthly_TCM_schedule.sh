c2c047092db0df538b4889b752a94df0bc71119a
danny316p <2865410+danny316p@users.noreply.github.com>
2025-07-09 21:44:46 -0400

    Update July

--- monthly_TCM_schedule.sh
+++ monthly_TCM_schedule.sh
@@ -4,7 +4,7 @@
 
 # use lynx to parse html and output to a file
 # DO NOT run this more than necessary, I don't know who runs this server and I don't want to abuse it
-lynx -dump http://escapepress.com/tcmsched/tcmbig_06.htm > tschlyn
+lynx -dump http://escapepress.com/tcmsched/tcmbig_07.htm > tschlyn
 
 # mangle that file with sed and grep to create a Letterboxd-ready csv file
 echo "Title, Year" > sedoutput.csv; grep "\[[0-9]" tschlyn | grep -v "TCM Schedule" | grep -v "web counter" | sed -E 's/^.*\](.*) \(([1-2][0-9][0-9][0-9])\)/\"\1\", \2/' | sed -E 's/(^.*\])//' >> sedoutput.csv
