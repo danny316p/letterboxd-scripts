580907001d5f78a58838714dbf07803e6a45ca5f
danny316p <2865410+danny316p@users.noreply.github.com>
2025-09-13 11:59:14 -0400

    initial commit of passable MeTV script

---
+++ metv_saturday_cartoons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#bash commands to download a copy of the weekly MeTV Saturday Morning Cartoons schedule and convert it to Letterboxd list format
+# I have not bothered to make this all that generic...
+
+# use lynx to parse html and output to a file
+# DO NOT run this more than necessary, I don't want to abuse MeTV's server
+lynx -dump https://www.metv.com/schedule/2025-09-13 > mschlyn
+
+# mangle that file with sed and grep to create a Letterboxd-ready csv file
+echo "Title" > sedoutput.csv
+#grep -n "10:00am" mschlyn | awk -F: '{print $1}' | xargs head mschlyn -n | sed '1,/7:00am/d' | sed '/\[/d' | sed '/0am/d' >> sedoutput.csv
+sed '/  10:00am/,$d' mschlyn | sed '1,/7:00am/d' | sed '/\[/d' | sed '/0am/d' | tr -d '\r\n' | sed 's/"[^"]*"//g' | sed 's/,/\n/g' | sed 's/     /\n/g' >> sedoutput.csv
+
